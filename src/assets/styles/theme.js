43490a33cb82ae5df448688d9ebe9331eca317fe
Grzegorz <g.ratajczak97@gmail.com>
2022-02-12 14:13:43 +0100

    added new colors

--- src/assets/styles/theme.js
+++ src/assets/styles/theme.js
@@ -1,17 +1,19 @@
 export const theme = {
-    colors: {
-        white: '#FFFFFF',
-        lightGrey: '#F7F8FA',
-        darkGrey: '#737C8E',
-        grey: '#C0C7D6',
-        black: '#111111',
-        success: '#8FCB81',
-        error: '#CB8581',
-        warning: '#E1D888',
-    },
-    fontSize: {
-        l: '22px',
-        m: '14px',
-        s: '12px',
-    },
+  colors: {
+    white: '#FFFFFF',
+    lightGrey: '#F7F8FA',
+    darkGrey: '#737C8E',
+    grey: '#C0C7D6',
+    black: '#111111',
+    success: '#8FCB81',
+    error: '#CB8581',
+    warning: '#E1D888',
+    darkPurple: '#C0C7D6',
+    lightPurple: '#ECEFF7',
+  },
+  fontSize: {
+    l: '22px',
+    m: '14px',
+    s: '12px',
+  },
 };
