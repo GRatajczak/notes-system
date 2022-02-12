1d70d3c4d4121b59b4b2dd6c8c1a534166fd0dbc
Grzegorz <g.ratajczak97@gmail.com>
2022-02-12 14:16:00 +0100

    added msw to index and mock rest api

--- src/index.js
+++ src/index.js
@@ -5,12 +5,15 @@ import Root from 'views/Root';
 import { worker } from 'mocks/browser';
 
 if (process.env.NODE_ENV === 'development') {
-  worker.start().catch((err) => console.log(err));
+  worker
+    .start()
+    .then(() => {
+      ReactDOM.render(
+        <React.StrictMode>
+          <Root />
+        </React.StrictMode>,
+        document.getElementById('root')
+      );
+    })
+    .catch((err) => console.log(err));
 }
-
-ReactDOM.render(
-  <React.StrictMode>
-    <Root />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
