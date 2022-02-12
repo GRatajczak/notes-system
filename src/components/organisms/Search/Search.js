4e59c308befc6ee8860149e88bd024fd4124a2a2
Grzegorz <g.ratajczak97@gmail.com>
2022-02-12 14:15:21 +0100

    added downshift to Search component and added styled components

--- src/components/organisms/Search/Search.js
+++ src/components/organisms/Search/Search.js
@@ -1,22 +1,63 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Input } from 'components/atoms/Input/Input';
-import { StyledWrapper, StatusInfo } from './Search.styles';
+import { useCombobox } from 'downshift';
+import {
+  StyledWrapper,
+  StatusInfo,
+  SearchResults,
+  StyledSearchWrapper,
+  SearchResultsItem,
+} from './Search.styles';
+import { useUsers } from 'hooks/useUsers';
+import debounce from 'lodash.debounce';
 
 const Search = (props) => {
-    return( 
-    <StyledWrapper>
-        <StatusInfo>
-            <p>
-                Logged as:
-            </p>
-            <strong>
-                Teacher
-            </strong>
-        </StatusInfo>
-        <Input search placeholder='Find what you want'/>
-    </StyledWrapper>
-    );
-};
+  const [matchingStudents, setMatchingStudents] = useState([]);
+  const { findStudents } = useUsers();
 
+  const getMatchingStudents = debounce(async (e) => {
+    console.log(e.inputValue);
+    const { students } = await findStudents(e.inputValue);
+    setMatchingStudents(students);
+  }, 500);
+
+  const {
+    isOpen,
+    getToggleButtonProps,
+    getLabelProps,
+    getMenuProps,
+    getInputProps,
+    getComboboxProps,
+    highlightedIndex,
+    getItemProps,
+  } = useCombobox({
+    items: matchingStudents,
+    onInputValueChange: getMatchingStudents,
+  });
+
+  return (
+    <StyledWrapper>
+      <StatusInfo>
+        <p>Logged as:</p>
+        <strong>Teacher</strong>
+      </StatusInfo>
+      <StyledSearchWrapper {...getComboboxProps()}>
+        <Input {...getInputProps()} search placeholder="Find what you want" />
+        <SearchResults isOpen={isOpen} {...getMenuProps()}>
+          {isOpen &&
+            matchingStudents.map((item, index) => (
+              <SearchResultsItem
+                highLighted={highlightedIndex === index}
+                {...getItemProps({ item, index })}
+                key={item.id}
+              >
+                {item.name}
+              </SearchResultsItem>
+            ))}
+        </SearchResults>
+      </StyledSearchWrapper>
+    </StyledWrapper>
+  );
+};
 
 export default Search;
--- src/components/organisms/Search/Search.styles.js
+++ src/components/organisms/Search/Search.styles.js
@@ -1,23 +1,64 @@
-import { Input } from "components/atoms/Input/Input";
-import styled from "styled-components";
+import { Input } from 'components/atoms/Input/Input';
+import styled from 'styled-components';
 
 export const StyledWrapper = styled.div`
-    padding: 20px 30px 20px;
-    border-bottom: 1px solid ${({theme}) => theme.colors.grey};
-    display: flex;
-    justify-content: start;
+  padding: 20px 30px 20px;
+  border-bottom: 1px solid ${({ theme }) => theme.colors.grey};
+  display: flex;
+  justify-content: start;
+  ${Input} {
+    font-size: 18px;
+    width: 100%;
+    max-width: 350px;
+    border: none;
+    box-shadow: 0 0 10px ${({ theme }) => theme.colors.grey};
+  }
+`;
 
-    ${Input}{
-        font-size: 18px;
-        width: 100%;
-        max-width: 350px;
-        border: none;
-        box-shadow: 0 0 10px ${({theme}) => theme.colors.grey};
-    }
+export const StyledSearchWrapper = styled.div`
+  position: relative;
 `;
 
 export const StatusInfo = styled.div`
-    display: flex;
-    flex-direction: column;
-    margin-right: 20px;
+  display: flex;
+  flex-direction: column;
+  margin-right: 20px;
+`;
+
+export const SearchResults = styled.ul`
+  opacity: ${({ isOpen }) => (isOpen ? '1' : '0')};
+  transition: opacity .3s .3s, visibility:  .3s ;
+  visibility: ${({ isOpen }) => (isOpen ? 'visible' : 'hidden')};
+  z-index: 1000;
+  max-height: 500px;
+  overflow-y: scroll;
+  padding: 10px;
+  border-radius: 15px;
+  list-style: none;
+  width: 100%;
+  position: absolute;
+  left: 0;
+  top: 100%;
+  display: flex;
+  flex-direction: column;
+  background-color: ${({ theme }) => theme.colors.white};
+`;
+
+export const SearchResultsItem = styled.li`
+  font-weight: bold;
+  color: ${({ theme }) => theme.colors.darkGrey};
+  background-color: ${({ theme }) => theme.colors.white};
+  width: 100%;
+  font-size: 16px;
+  padding: 20px 5px;
+  &:hover,
+  &:focus {
+    background-color: ${({ theme }) => theme.colors.lightPurple};
+  }
+  background-color: ${({ theme, highLighted }) =>
+    highLighted ? theme.colors.lightPurple : theme.colors.white};
+
+  &:not(:last-child) {
+    border-bottom: 1px solid ${({ theme }) => theme.colors.darkPurple};
+  }
 `;
